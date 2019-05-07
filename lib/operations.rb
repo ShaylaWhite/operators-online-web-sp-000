13d44b4d1474a1e68849f631433374eafb3bddf0
Shayla White <shayladwhite1@gmail.com>
2019-05-07 04:47:07 +0000

    Automatically backed up by Learn

--- lib/operations.rb
+++ lib/operations.rb
@@ -1,10 +1,9 @@
 def unsafe?(speed)
-  if  speed < 40 && speed > 60
-    return true
- else speed <= 40 && speed >= 60
-   return false
-
-end
+ if speed > 60 || speed < 40
+		return true;
+	else
+		return false;
+	end
 end
 
 
